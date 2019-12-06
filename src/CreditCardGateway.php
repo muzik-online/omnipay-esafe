7dad65f44a4a15c3d7821dbe75e0dbe194ed2d57
Vincent Chi <song374561@chivincent.net>
2019-12-06 14:56:50 +0800

    feat: add refund feature for credit card and unionpay gateway.

--- src/CreditCardGateway.php
+++ src/CreditCardGateway.php
@@ -2,10 +2,20 @@
 
 namespace Muzik\OmnipayEsafe;
 
+use Muzik\OmnipayEsafe\Message\RefundRequest;
+
 class CreditCardGateway extends AbstractGateway
 {
     public function getName()
     {
         return 'ESafe Credit Card';
+    }
+
+    public function refund(array $options = array())
+    {
+        $request = new RefundRequest();
+        $request->initialize($options);
+
+        return $request;
     }
 }
--- src/UnionpayGateway.php
+++ src/UnionpayGateway.php
@@ -2,10 +2,20 @@
 
 namespace Muzik\OmnipayEsafe;
 
+use Muzik\OmnipayEsafe\Message\RefundRequest;
+
 class UnionpayGateway extends AbstractGateway
 {
     public function getName()
     {
         return 'ESafe Unionpay';
+    }
+
+    public function refund(array $options = array())
+    {
+        $request = new RefundRequest();
+        $request->initialize($options);
+
+        return $request;
     }
 }
